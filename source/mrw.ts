f15178e38cade47e559f34864d0bdab90eaa1790
Alisue <lambdalisue@gmail.com>
2024-03-11 01:38:20 +0900

    First

---
+++ source/mrw.ts
@@ -0,0 +1,25 @@
+import type { Source } from "https://deno.land/x/fall_core@v0.3.0/mod.ts";
+import { assert, is } from "https://deno.land/x/unknownutil@v3.16.3/mod.ts";
+
+const isOptions = is.StrictOf(is.PartialOf(is.ObjectOf({})));
+
+export function getSource(
+  options: Record<string, unknown>,
+): Source {
+  assert(options, isOptions);
+  return {
+    getStream: async (denops, ..._args: string[]) => {
+      const paths = await denops.call("mr#mrw#list") as string[];
+      return ReadableStream.from(paths).pipeThrough(
+        new TransformStream({
+          transform(chunk, controller) {
+            controller.enqueue({
+              value: chunk,
+              detail: { path: chunk },
+            });
+          },
+        }),
+      );
+    },
+  };
+}
